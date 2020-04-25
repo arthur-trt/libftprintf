c62308ddad4a600de59e02c338263cf3f0e63936
Arthur <arthur@sperec.fr>
2020-04-25 14:29:30 +0200

    Rename .h for include in another porject

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: atrouill <atrouill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 19:45:28 by atrouill          #+#    #+#             */
-/*   Updated: 2020/04/06 17:59:41 by atrouill         ###   ########.fr       */
+/*   Updated: 2020/04/25 14:29:04 by atrouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 # include <unistd.h>
 # include <stdarg.h>
 # include <stdlib.h>
-# include "constants.h"
-# include "structure.h"
-# include "functions.h"
+# include "ft_printf_constants.h"
+# include "ft_printf_structure.h"
+# include "ft_printf_functions.h"
 
 int	ft_printf(const char *format, ...);
 
