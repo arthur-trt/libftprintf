66825776fb233b30fa752b3d05ed3b6a615518ec
Arthur-TRT <atrouill@student.42.fr>
2021-07-23 15:14:47 +0200

    Change from void * to char *

--- obj/ft_memdel.d
+++
@@ -1,3 +0,0 @@
-obj/ft_memdel.o: srcs/ft_memdel.c srcs/../includes/libft.h
-srcs/ft_memdel.c:
-srcs/../includes/libft.h:
--- obj/ft_realloc.d
+++
@@ -1,3 +0,0 @@
-obj/ft_realloc.o: srcs/ft_realloc.c srcs/../includes/libft.h
-srcs/ft_realloc.c:
-srcs/../includes/libft.h:
